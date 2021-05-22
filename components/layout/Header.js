947ddef14672c77d32e2e9e7e62b82d5419a5b0f
Samuel Guardado <grd.rodrigez13@gmail.com>
2021-05-22 17:15:22 -0600

    first structure

---
+++ components/layout/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavBar from './NavBar';
+const Header = () => {
+    return ( <div>
+        <NavBar />
+    </div> );
+}
+ 
+export default Header;
---
+++ components/layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from "./Header"
+
+const Layout = () => {
+    return ( <div className="gradient">
+        <Header />
+    </div> );
+}
+ 
+export default Layout;
