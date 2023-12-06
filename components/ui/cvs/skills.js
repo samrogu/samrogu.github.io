07055075ce82a59374274725b7f1f949def9973a
Samuel Guardado <grd.rodrigez13@gmail.com>
2023-12-06 01:39:57 -0600

    "POR-6 Solventado error de compilacion"

--- components/ui/cvs/skills.js
+++ components/ui/cvs/skills.js
@@ -4,11 +4,11 @@ import ListItem from './list_item';
 
 const Skills = ({ data }) => (
   <section className="border-b py-8">
-    <div class="container mx-auto flex flex-wrap pt-4 pb-12 lg:flex-row justify-center">
+    <div className="container mx-auto flex flex-wrap pt-4 pb-12 lg:flex-row justify-center">
     <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center">Skills</h1>
     {data.map(skill => (
       <div key={skill.title} className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-        <div class="flex-1 rounded-t rounded-b-none overflow-hidden shadow">
+        <div className="flex-1 rounded-t rounded-b-none overflow-hidden shadow">
         {skill.type === 'percent' && (
               <h1 className="">
               {skill.title}
